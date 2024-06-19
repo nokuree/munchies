1eb2559efb0f46bcf2d33ba2b57789e466889ebe
nokuree <meilisimon378@gmail.com>
2024-06-19 12:34:10 -0700

    Signup code and Authentication, might modify later to include Auth0 but I just want a basic auth system to begin with

---
+++ frontend/src/components/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Signup from "./Signup"
+import { Container } from "react-bootstrap"
+import {AuthProvider} from "../contexts/AuthContext";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import axios from 'axios';
+// funny routing and rendering stuff, like when we minimize the window the application wont just implode on itself 
+function App() {
+    return ( 
+
+        <Container 
+            className="d-flex align-items-center justify-content-center"
+            style={{ minHeight: "100vh"}}
+        >   
+            <div className="w-100" style={{maxWidth: "400px"}}>
+                <Router>
+                    <AuthProvider>
+                        <Routes>
+                            <Route path="/signup" element={<Signup/>}/>
+                        </Routes>
+                    </AuthProvider>
+                </Router>
+            </div>
+        </Container>
+
+    )
+}
+
+export default App;
+
---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,57 @@
+// This AuthContext provides the SignUp logic when you put in your email and password when signing up, so it communicates
+// with firebase and stores user email and password
+
+
+import React, { useContext, useState, useEffect } from 'react';
+import {auth} from "../firebase"
+import { createUserWithEmailAndPassword,onAuthStateChanged } from 'firebase/auth';
+
+// Creates a React context, this context will store the authentication state and functions
+const AuthContext = React.createContext()
+
+// Uses useContext to access the value stored in AuthContext, which is the authentication state and functions, we dont need to 
+// directly pass them as props through the component tree yay
+export function useAuth() {
+    return useContext(AuthContext)
+}
+
+// Manages the authentication state and provides functions to the child components
+export function AuthProvider({children}) {
+    // the state variables we will use later
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true)
+
+    // the signup function takes email and passwords 
+    function signup(email,password){
+        return createUserWithEmailAndPassword(auth, email,password)
+    }
+
+    // useEffect hook: Attatches a listener to the auth state using onAuthStateChanged, and when the auth state logs in or out, it updates 
+    // the currentUser state with new user information, and sets loading to false to indicate that the state is fetched, then it returns unsubscruibe which removes the 
+    // listener so no memory leaks 
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setCurrentUser(user)
+            setLoading(false)
+            
+        });
+        return unsubscribe
+    }, []);
+
+    // A little redundent but eh :p
+    onAuthStateChanged(auth, user => {
+        setCurrentUser(user)
+    })
+
+    // Contains the data we use for the functions, currentUser and signup 
+    const value = {
+        currentUser,
+        signup
+    } // Only renders the children, so if loading is false which means the authentication sate is fetched, it wont render the app until the users authentication state
+    // is known. 
+    return (
+      <AuthContext.Provider value={value}>
+            {!loading && children}
+      </AuthContext.Provider>
+    )
+}
